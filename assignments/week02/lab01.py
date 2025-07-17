34eb6fee459fc49a050f4189695486d1b54faf97
Phongpranot Boonmak <Phongpranot.b@ku.th>
2025-07-17 15:34:03 +0700

    Hon20

--- assignments/week02/lab01.py
+++ assignments/week02/lab01.py
@@ -10,7 +10,24 @@ Shows BMI category based on the ranges below
 
 BMI Categories:
 
-Below 18.5: Underweight
-18.5 - 24.9: Normal weight
-25.0 - 29.9: Overweight
-30.0 and above: Obese
+Below 19.5: Underweight
+20.5 - 25.9: Normal weight
+26.0 - 30.9: Overweight
+31.0 and above: Obese
+
+***
+
+
+weight = float(input("Weight: "))
+height = float(input("Height: "))
+bmi = weight / height ** 2
+print("Your BMI: ", bmi)
+if bmi < 19.5:
+    print("Underweight")
+ 
+if bmi >= 20.5 and bmi <= 25.9:
+    print("Normal Weight")
+if bmi >= 26.0 and bmi <= 30.9:
+    print("Overweight Weight")
+if bmi >=31.0:
+    print("Obese")
--- assignments/week02/lab02.py
+++ assignments/week02/lab02.py
@@ -8,3 +8,13 @@ Ask for the amount to convert
 Use exchange rate: 1 USD = 35.5 THB
 Display result with 2 decimal places
 Show the calculation formula used
+
+***
+
+choice = input("1 THB -> USD, 2USD -> THB")
+amount = float (input ("Amount: "))
+if choice == "1":
+print(f"{amount} THB = {amount/35.5} USD")
+if choice =="2":
+   print(f"{amount} USD = {amount * 35.5} THB ")
+ 
