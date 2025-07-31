1c7d62a58a49599e97dccda87a8dffbf7680cf6a
Phongpranot Boonmak <Phongpranot.b@ku.th>
2025-07-31 15:23:02 +0700

    Hon20

--- homeworks/hw1/hw1-expense-calculator.py
+++ homeworks/hw1/hw1-expense-calculator.py
@@ -1,20 +1,20 @@
 """
 Personal Finance Calculator
-Student: [Your Name]
-Date: [Today's Date]
+Student: [Phongpranot Boonmak]
+Date: [27/07/2568]
 Purpose: Calculate monthly budget and savings
 """
 
-# รับค่าข้อมูล
+
 monthly_income = float(input("Enter your monthly income (THB): "))
 rent_cost = float(input("Enter your monthly rent/housing cost (THB): "))
 food_budget = int(input("Enter your monthly food budget (THB): "))
 transportation_cost = float(input("Enter your monthly transportation cost (THB): "))
 entertainment_budget = int(input("Enter your monthly entertainment budget (THB): "))
 emergency_fund_percent = float(input("Enter emergency fund percentage: "))
-investment_percent = float(input("Enter investment percentage:(e.g.15) "))
+investment_percent = float(input("Enter investment percentage: "))
 
-# คำนวณ
+
 fixed_expenses = rent_cost + transportation_cost
 variable_expenses = food_budget + entertainment_budget
 total_expenses = fixed_expenses + variable_expenses
@@ -24,7 +24,6 @@ investment_amount = monthly_income * (investment_percent / 100)
 available_savings = remaining_income - emergency_fund - investment_amount
 expense_ratio = (total_expenses / monthly_income) * 100
 
-# แสดงผล
 print("\n=== MONTHLY BUDGET REPORT ===")
 print(f"Income: {monthly_income:.2f} THB")
 print(f"Fixed Expenses: {fixed_expenses:.2f} THB")
